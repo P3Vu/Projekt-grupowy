2a71a9efef7f3c2cd8c8b86d1a46ed12da3aab6d
Pawel Wojciechowski <p3wojciechowski@gmail.com>
2020-05-04 11:14:09 +0200

    Dodaje pelny projekt z czujnikiem pylu PMS3003

---
+++ Czujnik pylu - PMS3003/PMS/SW4STM32/PMS/Debug/Application/User/main.d
@@ -0,0 +1,83 @@
+Application/User/main.o: \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Src/main.c \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Inc/main.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Inc/main.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Inc/main.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Inc/main.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/pawel/Desktop/Studia/mgr/Projekt\ grupowy/Projekt/PMS/PMS/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
